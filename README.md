b94e288e985bb826f2ed4ed6b3c232e0246d0430
Arthur Miranda <drxmirand@gmail.com>
2026-04-07 10:05:16 -0300

    Perguntas 1-5

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Prova-bossinip1
 # Membros
 Arthur Silva Miranda
-
+RA:2041382611039
 Lucas Rodrigues Pinho
-
+RA:
 Marcelo Augusto luvizutto
+RA:
---
+++ script.sql
@@ -0,0 +1,59 @@
+-- Enunciado 1 - Base de dados e criação de tabela
+
+-- A base a ser utilizada pode ser obtida a partir do link a seguir.
+-- https://www.kaggle.com/datasets/yasserh/titanic-dataset
+-- Ela deve ser importada para uma base de dados gerenciada pelo PostgreSQL. Os dados
+-- devem ser armazenados em uma tabela apropriada para as análises desejadas. Você deve
+-- identificar as colunas necessárias, de acordo com a descrição de cada item da prova.
+-- Além, é claro, de uma chave primária (de auto incremento). Neste item, portanto, você
+-- deve desenvolver o script de criação da tabela.
+
+-- Mensagem de commit: feat(p1): cria base e importa dados
+
+
+
+
+
+
+
+
+
+
+
+-- Enunciado 2 - Sobrevivência em função da classe social
+-- Escreva um cursor não vinculado que mostra o número de passageiros sobreviventes que
+-- viajavam na primeira classe (Pclass = 1).
+-- Mensagem de commit: feat(p1): encontra sobreviventes da primeira classe
+
+
+
+
+
+
+
+-- Enunciado 3 - Sobrevivência em função do gênero
+-- Escreva um cursor com query dinâmica que mostra o número de passageiros
+-- sobreviventes dentre as mulheres (Sex = 'female'). Escreva um condicional para que, se
+-- não existir nenhuma, o valor -1 seja exibido.
+-- Mensagem de commit: feat(p1): encontra sobreviventes do sexo feminino
+
+
+
+
+
+-- Enunciado 4 - Tarifa versus embarque
+-- Dentre os passageiros que pagaram tarifa (Fare) maior que 50, quantos embarcaram em
+-- Cherbourg (Embarked = 'C')? Escreva um cursor vinculado que exiba esse valor.
+-- Mensagem de commit: feat(p1): encontra passageiros de Cherbourg com tarifa alta
+
+
+
+
+
+
+-- Enunciado 5 Limpeza de valores NULL
+-- Escreva um cursor não vinculado para a remoção de todas as tuplas que possuam o valor
+-- NULL em pelo menos um de seus campos. Antes de fazer a sua remoção, exiba a tupla. A
+-- seguir, mostre as tuplas remanescentes, de baixo para cima.
+-- Mensagem de commit: feat(p1): remove com dados faltantes
+
